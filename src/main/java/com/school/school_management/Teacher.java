83b1da923fefd9eff216526ad546133a6716bf3d
Abdulrahim <abdulrahim.jalloh.1710@gmail.com>
2026-05-03 00:38:48 +0000

    Constructor and getter & setter added to teacher

--- src/main/java/com/school/school_management/Teacher.java
+++ src/main/java/com/school/school_management/Teacher.java
@@ -32,4 +32,53 @@ public class Teacher {
    //Relationship: One teacher teaches Many courses
    @OneToMany(mappedBy = "teacher")
    private Set<Course> courses = new HashSet<>();
+
+   public Teacher () {}
+
+   public Teacher(String name, String email, String specification, Set<Course> courses) {
+      this.name = name;
+      this.email = email;
+      this.specification = specification;
+      this.courses = courses;
+   }
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public String getSpecification() {
+      return specification;
+   }
+
+   public void setSpecification(String specification) {
+      this.specification = specification;
+   }
+
+   public Set<Course> getCourses() {
+      return courses;
+   }
+
+   public void setCourses(Set<Course> courses) {
+      this.courses = courses;
+   }
 }
