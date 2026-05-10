c55791d8a08746b0ea3d5ed01501825f32c1ce5e
Abdulrahim <abdulrahim.jalloh.1710@gmail.com>
2026-05-10 02:05:05 +0000

    updated studentRepo with pagination

--- src/main/java/com/school/school_management/student/StudentRepository.java
+++ src/main/java/com/school/school_management/student/StudentRepository.java
@@ -1,11 +1,24 @@
 package com.school.school_management.student;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
 import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
    Optional<Student> findByEmail(String Email);
+
+   //Paginated version of findAll
+   //spring data Jpa handles everything
+   Page<Student> findAll(Pageable pageable);
+
+   //paginated search by name
+   Page<Student> findByNameContaining(String name, Pageable pageable);
+
+   //paginated search by age greater than
+   Page<Student> findByAgeGreaterThan(int age, Pageable pageable);
+
 }
