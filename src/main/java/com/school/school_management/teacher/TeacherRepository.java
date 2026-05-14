96e80867d7782540221b55c066f6920147d36a71
Abdulrahim <abdulrahim.jalloh.1710@gmail.com>
2026-05-14 13:27:51 +0000

    added pagination repo for teacherRepo

--- src/main/java/com/school/school_management/teacher/TeacherRepository.java
+++ src/main/java/com/school/school_management/teacher/TeacherRepository.java
@@ -1,12 +1,16 @@
 package com.school.school_management.teacher;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
 import java.util.Optional;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 
    Optional<Teacher> findByEmail(String email);
+
+   Page<Teacher> findAll(Pageable pageable);
 }
