31238525f2446295bda87c43a9ab8648687c496e
Abdulrahim <abdulrahim.jalloh.1710@gmail.com>
2026-06-06 23:06:45 +0000

    added auth provider and auth manager

--- src/main/java/com/school/school_management/securityConfig/SecurityConfig.java
+++ src/main/java/com/school/school_management/securityConfig/SecurityConfig.java
@@ -5,9 +5,15 @@ import com.school.school_management.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -68,5 +74,26 @@ public class SecurityConfig {
               );
 
       return http.build();
+   }
+
+   //PASSWORD ENCODER (BCrypt)
+   @Bean
+   public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+   }
+
+   //AUTHENTICATION PROVIDER
+   @Bean
+   public AuthenticationProvider authenticationProvider() {
+      DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+      provider.setUserDetailsService(userDetailsService);
+      provider.setPasswordEncoder(passwordEncoder());
+      return provider;
+   }
+
+   //AUTHENTICATION MANAGER
+   @Bean
+   public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+      return config.getAuthenticationManager();
    }
 }
