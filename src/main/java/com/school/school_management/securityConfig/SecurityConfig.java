e1f9816afabf3cd564bf3a4d0b4951d2703ecbce
Abdulrahim <abdulrahim.jalloh.1710@gmail.com>
2026-06-06 22:50:11 +0000

    create security config file and implemented the security rules

---
+++ src/main/java/com/school/school_management/securityConfig/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.school.school_management.securityConfig;
+
+import com.school.school_management.jwt.JwtAuthenticationFilter;
+import com.school.school_management.service.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+   private final CustomUserDetailsService userDetailsService;
+   private final JwtAuthenticationFilter jwtAuthFilter;
+
+   public SecurityConfig(CustomUserDetailsService userDetailsService, JwtAuthenticationFilter jwtAuthFilter) {
+      this.userDetailsService = userDetailsService;
+      this.jwtAuthFilter = jwtAuthFilter;
+   }
+
+   //SECURITY RULES
+   public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+
+      http
+              .csrf(csrf -> csrf.disable())
+              .authorizeHttpRequests(auth -> auth
+                      .requestMatchers("/api/auth/**").permitAll()
+                      .requestMatchers(HttpMethod.POST,
+                              "/api/students/**",
+                              "/api/courses/**",
+                              "/api/teachers/**"
+                      ).hasRole("ADMIN")
+
+                      .requestMatchers(HttpMethod.PUT,
+                              "/api/students/**",
+                              "/api/courses/**",
+                              "/api/teachers/**"
+                      ).hasRole("ADMIN")
+
+                      .requestMatchers(HttpMethod.DELETE,
+                              "/api/students/**",
+                              "/api/courses/**",
+                              "/api/teachers/**"
+                      ).hasRole("ADMIN")
+
+                      .anyRequest().authenticated()
+              )
+
+              //use stateless sessions (jwt, not cookies)
+              //spring boot wont store sessions
+              //every request must have jwt token
+              .sessionManagement(session -> session
+                      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+              )
+
+              //user authentication provider
+              .authenticationProvider(authenticationProvider())
+
+              //add jwt filter before spring's default filter
+              .addFilterBefore(
+                      jwtAuthFilter,
+                      UsernamePasswordAuthenticationFilter.class
+              );
+
+      return http.build();
+   }
+}
