5213ea7cf68c74e464dc7621826bb3ea5c130ddd
Abdulrahim <abdulrahim.jalloh.1710@gmail.com>
2026-08-26 03:25:07 +0000

    enables scheduling in the main method

--- src/main/java/com/school/school_management/SchoolManagementApplication.java
+++ src/main/java/com/school/school_management/SchoolManagementApplication.java
@@ -8,10 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching  //This activates caching
 @EnableAsync
+@EnableScheduling
 public class SchoolManagementApplication {
 
 	public static void main(String[] args) {
